9cff0f1a808223a5a12f7dbe8c75f6c8c540d86e
Reinaldo Rossetti <reinaldo.rossetti@outlook.com>
2022-12-30 11:51:45 -0300

    ajustes para exemplo

--- features/step_definitions/steps_tests.rb
+++ features/step_definitions/steps_tests.rb
@@ -2,17 +2,23 @@
 #language: pt
 
 Dado('que esteja na pagina desejada') do
-  visit '/'
-  # usando xpath e text.
-  find(:xpath, '//a[text()="Começar Automação Web"]', visible: true).click
-  # usando css que é padrão (não precisa colocar o tipo) e text.
-  find('a', text: 'Formulário', visible: true).click
+  begin
+    visit '/'
+    # usando xpath e text.
+    find(:xpath, '//a[text()="Começar Automação Web"]', visible: true).click
+    # usando css que é padrão (não precisa colocar o tipo) e text.
+    find('a', text: 'Formulário', visible: true).click
+  rescue
+    puts "Deu BO no Script, veja se o site esta disponivel!!"
+    $erro = true
+  end
 end
 
-Quando('realizo o cadastro das informações {string} e {string}') do |string, string2|
-  print "#TODO"
+Quando('realizo o cadastro das informações {string} e {string}') do |nome, email|
+  print "Nome: #{nome}, Email: #{email}"
 end
 
-Então('valido o {string} e a sua mensagem de erro {string}') do |string, string2|
-  print "#TODO"
+Então('valido o {string} e a sua mensagem de erro {string}') do |mensagem, erro|
+  print "Nome: #{mensagem}, Email: #{erro}"
+  if $erro then fail  ArgumentError, 'Teste Falhou' end
 end
